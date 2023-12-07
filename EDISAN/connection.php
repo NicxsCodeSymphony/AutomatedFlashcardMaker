bb202d5690241691dae32c6b2d083f6f01f29263
John Nico M. Edisan <nicxsician@gmail.com>
2023-12-07 11:31:47 +0800

    Delete EDISAN directory

--- EDISAN/connection.php
+++
@@ -1,8 +0,0 @@
-<?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "flashcard";  
-      
-    $con = mysqli_connect($host,$user,$password,$db_name);
-?>  
